2e2a415a23d13fe077d4b467b4d7fa8113bdc21e
Bear Ridge <ahuss2010@live.com>
2022-02-21 15:11:09 -0600

    update

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,9 +13,27 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const connectDb = await openDB('jate', 1);
+  const txt = connectDb.transaction('jate', 'readwrite');
+  const obStore = txt.objectStore('jate');
+  const request = obStore.add({ content: content });
+  const result = await request;
+  console.log('content saved to the database', result);
+  // console.error('putDb not implemented');
+};
+
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const connectDb = await openDB('jate', 1);
+  const txt = connectDb.transaction('jate', 'readonly');
+  const obStore = txt.objectStore('jate');
+  const request = obStore.getAll();
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+  // console.error('getDb not implemented');
+};
 
 initdb();
